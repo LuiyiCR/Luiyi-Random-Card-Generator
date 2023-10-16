5d620667e5019c8dbbbdd145f6dbbc4a00a0a42f
Luis <luiyi_salazar@hotmail.com>
2023-10-15 19:47:48 -0600

    set-interval timer

--- randomCardGenerator.js
+++ randomCardGenerator.js
@@ -1,32 +1,40 @@
-const suitTop = document.querySelector('.suit-top');
-const suitBottom = document.querySelector('.suit-bottom');
-const number = document.querySelector('.card-number');
+function generateCard() {
+  const suitTop = document.querySelector('.suit-top');
+  const suitBottom = document.querySelector('.suit-bottom');
+  const number = document.querySelector('.card-number');
 
-const numbers = [
-  'A',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  'J',
-  'Q',
-  'K',
-];
+  const numbers = [
+    'A',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'J',
+    'Q',
+    'K',
+  ];
 
-const suits = ['&#9824', '&#9827', '&#9829', '&#9830'];
-const randomNumber = Math.floor(Math.random() * numbers.length);
-const randomSuit = Math.floor(Math.random() * suits.length);
-const suitColor = randomSuit === 2 || randomSuit === 3 ? 'red' : 'black';
+  const suits = ['&#9824', '&#9827', '&#9829', '&#9830'];
+  const randomNumber = Math.floor(Math.random() * numbers.length);
+  const randomSuit = Math.floor(Math.random() * suits.length);
+  const suitColor = randomSuit === 2 || randomSuit === 3 ? 'red' : 'black';
 
-suitTop.innerHTML = suits[randomSuit];
-suitBottom.innerHTML = suits[randomSuit];
-suitTop.style.color = suitColor;
-suitBottom.style.color = suitColor;
+  suitTop.innerHTML = suits[randomSuit];
+  suitBottom.innerHTML = suits[randomSuit];
+  suitTop.style.color = suitColor;
+  suitBottom.style.color = suitColor;
 
-number.innerHTML = numbers[randomNumber];
+  number.innerHTML = numbers[randomNumber];
+}
+
+generateCard();
+
+// setInterval(() => {
+//   generateCard();
+// }, 5000);
